3841b7a8f7b04a1b10eeb45ecfa8acbde9a24b8f
NicolasStigler <166185995+NicolasStigler@users.noreply.github.com>
2024-11-11 03:40:38 +0000

    Update scrap_sismos.py

--- scrap_sismos.py
+++ scrap_sismos.py
@@ -1,36 +1,53 @@
-from requests_html import HTMLSession
+import requests
+from bs4 import BeautifulSoup
 import boto3
 import uuid
 
 def lambda_handler(event, context):
-    session = HTMLSession()
     url = "https://ultimosismo.igp.gob.pe/ultimo-sismo/sismos-reportados"
-    response = session.get(url)
-    response.html.render()  # Renderizar JavaScript
-
-    # Seleccionar filas de la tabla
-    rows = response.html.find('.table-hover tbody tr')
-
-    sismos = []
-    for row in rows[:10]:
-        columns = row.find('td')
-        sismo = {
-            'id': str(uuid.uuid4()),
-            'reporte': columns[0].text,
-            'referencia': columns[1].text,
-            'fecha_hora': columns[2].text,
-            'magnitud': columns[3].text
+    response = requests.get(url)
+    if response.status_code != 200:
+        return {
+            'statusCode': response.status_code,
+            'body': 'Error al acceder la página web'
         }
-        sismos.append(sismo)
+
+    # Parsear el contenido HTML de la pagina web
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # Seleccionar filas de la tabla
+    table = soup.find('table')
+    if not table:
+        return {
+            'statusCode': 404,
+            'body': 'No se encontro la tabla (posible rate limit)'
+        }
+
+    headers = [header.text.strip() for header in table.find_all('th')]
+    
+    sismos = []
+    for row in table.find_all('tr')[1:11]:
+        fields = row.find_all('td')
+        sismos.append({headers[i]: field.text.strip() for i, field in enumerate(fields)})
 
     # Configurar DynamoDB
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('TablaSismos')
 
-    # Insertar datos en DynamoDB
+    # Eliminar todos los elementos de la tabla antes de agregar los nuevos datos
+    scan = table.scan()
     with table.batch_writer() as batch:
-        for sismo in sismos:
-            batch.put_item(Item=sismo)
+        for each in scan['Items']:
+            batch.delete_item(
+                Key={
+                    'id': each['id']
+                }
+            )
+    
+    # Insertar datos en DynamoDB
+    for sismo in sismos:
+        sismo['id'] = str(uuid.uuid4())  # Generar un UUID para cada entrada
+        table.put_item(Item=sismo)
 
     return {
         'statusCode': 200,
